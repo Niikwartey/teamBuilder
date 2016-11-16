f8c0e5a202b4fbae0039d333e1e9715043d26c5a
paige11 <ellis.paige.v@gmail.com>
2016-11-16 14:38:14 -0500

    changed responses' answer column to boolean type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116191048) do
+ActiveRecord::Schema.define(version: 20161116192739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20161116191048) do
   create_table "responses", force: :cascade do |t|
     t.integer  "member_id"
     t.integer  "manager_id"
-    t.text     "answer"
+    t.boolean  "answer"
     t.integer  "importance"
     t.integer  "question_id"
     t.datetime "created_at",  null: false
