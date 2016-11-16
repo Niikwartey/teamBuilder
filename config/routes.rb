65c1eaaf6f2e5e27d4747954f8b28a817758dd15
paige11 <ellis.paige.v@gmail.com>
2016-11-16 17:37:00 -0500

    narrowed survey routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: "welcome#home"
-
-  resources :surveys
+  resources :surveys, only: [:new, :create]
 
 end
