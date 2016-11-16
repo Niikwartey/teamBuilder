a4b86f4a8f52ccbd33210ea1fa1f5dcda228eb9e
paige11 <ellis.paige.v@gmail.com>
2016-11-16 11:03:49 -0500

    added log out link to home page, created welcomecontroller, created root path

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :projects, as: :manager
-  has_many :project_members, as: :member
+  has_many :projects, as: :manager, class_name: User
+  has_many :project_members, as: :member, class_name: User
   has_many :projects, through: :project_members
   has_many :responses
   has_many :questions, through: :responses
