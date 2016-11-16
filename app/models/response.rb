9b32ab03151ea628ec709716c127f2153d4c5b80
jin501 <soljin01@gmail.com>
2016-11-16 12:12:17 -0500

    added survey model

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,5 @@ class Response < ApplicationRecord
   belongs_to :member, class_name: User
   belongs_to :manager, class_name: User
   belongs_to :question
+  belongs_to :survey
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116151629) do
+ActiveRecord::Schema.define(version: 20161116170218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 20161116151629) do
     t.integer  "question_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "survey_id"
+  end
+
+  create_table "surveys", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
