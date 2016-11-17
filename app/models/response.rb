dbc5b1ea4a3f31936aa7a8686e75cec66101a591
paige11 <ellis.paige.v@gmail.com>
2016-11-17 13:32:20 -0500

    got surveys/new form workinggit add .

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,5 @@
 class Response < ApplicationRecord
   belongs_to :user
   belongs_to :question
-  belongs_to :survey
+  belongs_to :survey, optional: true
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,4 +3,12 @@ class Survey < ApplicationRecord
   has_many :questions, through: :responses
 
   accepts_nested_attributes_for :responses
+
+  def responses_attributes=(attributes)
+    attributes.values.each do |attr|
+      response = Response.new(attr)
+      response.survey_id = self.id
+      self.responses << response
+    end
+  end
 end
