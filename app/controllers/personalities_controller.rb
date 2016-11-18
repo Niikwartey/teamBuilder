8748564df5a42f2c3d8c61ab43b08a0cd664e6c0
paige11 <ellis.paige.v@gmail.com>
2016-11-18 07:47:50 -0500

    added code to PersonalitiesController actions and created strong params

--- app/controllers/personalities_controller.rb
+++ app/controllers/personalities_controller.rb
@@ -1,9 +1,18 @@
 class PersonalitiesController < ApplicationController
 
   def new
+    @personality = Personality.new
   end
 
   def create
+    byebug
+    @personality = Personality.create(personality_params)
+  end
+
+  private
+
+  def personality_params
+    params.require(:personality).permit(:user_id, :intro_extroversion, :sensing_intuition, :thinking_feeling, :judging_perceiving)
   end
 
 end
