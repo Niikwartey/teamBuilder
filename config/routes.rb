1052aab45cf1ac21898ec17852d820e9729ead1c
jin501 <soljin01@gmail.com>
2016-11-17 19:35:43 -0500

    showall projects page, added delete button

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get '/projects', to: 'projects#showall'
 
+  delete '/users/:user_id/projects/:id', to: 'projects#destroy', as: :delete_project
 
   # Catch-all route, redirects to root
   match '*path', to: redirect('/'), via: :all
